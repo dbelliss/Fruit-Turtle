961ec851ee857a403056759b97f3c803078b2c0d
dbelliss <daveybelliss@yahoo.com>
2017-04-15 14:26:37 -0700

    Adding player animation and rotation

--- Assets/Thomas-Development/Scripts/PlayerMovement.cs
+++ Assets/Thomas-Development/Scripts/PlayerMovement.cs
@@ -6,15 +6,35 @@ public class PlayerMovement : MonoBehaviour {
 
     private Rigidbody2D playerRB;
     public float speed = 100.0f;
-
+	private Transform trans;
+	bool isMovingRight = true;
 	// Use this for initialization
 	void Start () {
+		
         playerRB = GetComponent<Rigidbody2D>();
+		trans = GetComponent<Transform> ();
 	}
-	
+
+	void flip() {
+		trans.localScale = new Vector3 (trans.localScale.x * -1, trans.localScale.y);
+		isMovingRight = !isMovingRight;
+	}
 	// Update is called once per frame
 	void Update () {
-		
+		Debug.Log (playerRB.velocity.x);
+		if (Input.GetAxis("Horizontal") > 0) {
+			if (isMovingRight == false) {
+				flip ();
+			}
+		}
+		else if (Input.GetAxis("Horizontal") < 0){
+			if (isMovingRight == true){
+				flip();
+			}
+		}
+		else {
+			//do not change
+		}
 	}
 
     private void FixedUpdate()
