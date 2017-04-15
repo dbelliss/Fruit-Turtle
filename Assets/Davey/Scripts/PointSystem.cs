43d57f715726e127d486dcab744a75145b95571d
dbelliss <daveybelliss@yahoo.com>
2017-04-15 13:41:51 -0700

    Adding point system

---
+++ Assets/Davey/Scripts/PointSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSystem : MonoBehaviour {
+
+	private EdgeCollider2D pointLine;
+	private int currentPoint;
+	public int pointToWin;
+
+	// Use this for initialization
+	void Start () {
+		pointLine = GetComponent<EdgeCollider2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (currentPoint >= pointToWin)
+		{
+			//finish game
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		Debug.Log("ENTERED");
+		if (collision.gameObject.tag == "PointItem")
+		{
+			currentPoint++;
+			Debug.Log(currentPoint);
+		}
+		if (collision.gameObject.tag == "BadItem")
+		{
+			currentPoint--;
+			Debug.Log(currentPoint);
+		}
+	}
+}
