e7abb37ca6a994d7f5c9517c42375965af645afc
Thomas Tran <thomastran878@gmail.com>
2017-04-15 12:51:39 -0700

    Added player, playermovement

---
+++ Assets/Thomas-Development/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D playerRB;
+    private float speed = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+        playerRB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float horizontalMovement;
+        Vector3 movement;
+
+        horizontalMovement = Input.GetAxis("Horizontal");
+
+        movement = new Vector3(horizontalMovement, 0.0f, 0.0f);
+
+        playerRB.velocity = movement * speed * Time.deltaTime;
+    }
+}
