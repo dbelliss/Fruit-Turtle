31d7d63ad33e082927f7370932bb8afdad20d7d3
dbelliss <daveybelliss@yahoo.com>
2017-04-15 16:35:42 -0700

    Highscore system

--- Assets/Davey/Scripts/GameManager.cs
+++ Assets/Davey/Scripts/GameManager.cs
@@ -2,21 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement; //reload level
 
 public class GameManager : MonoBehaviour {
 	static public int pointsToWin = 10;
 	static private int curPoints = 0;
 	static private Text scoreText;
+
 	// Use this for initialization
+
+
 	void Start () {
 		scoreText = GameObject.FindGameObjectWithTag ("scoreText").GetComponent<Text> ();
 		if (scoreText == null) {
 			Debug.Log ("Could not find score text");
 		}
+		printHighScores ();
+	}
+	void printHighScores() {
+		int[] highScores = new int[5];
+		for (int i= 0; i < highScores.Length; i++){
+
+			//Get the highScore from 1 - 5
+			string highScoreKey = "HighScore"+(i+1).ToString();
+			int highScore = PlayerPrefs.GetInt(highScoreKey,0);
+			Debug.Log (i.ToString() + " " + highScore.ToString());
+
+		}
 	}
 
 	void Update() {
-		Debug.Log (curPoints);
+		Debug.Log (curPoints); 
+		if (Input.GetAxis("Reset") != 0) {
+			updateScoreBoard ();
+			curPoints = 0;
+			Application.LoadLevel(Application.loadedLevel);
+		}
 	}
 
 	static void updatePoints(){
@@ -28,7 +49,30 @@ public class GameManager : MonoBehaviour {
 		curPoints += pts;
 		updatePoints ();
 		if (curPoints > pointsToWin) {
+			updateScoreBoard ();
 			Debug.Log ("YOU WIN");
+		}
+	}
+
+	static void updateScoreBoard() {
+		int[] highScores = new int[5];
+		int score = (int)Time.timeSinceLevelLoad;
+		for (int i= 0; i < highScores.Length; i++){
+			
+			//Get the highScore from 1 - 5
+			string highScoreKey = "HighScore"+(i+1).ToString();
+			int highScore = PlayerPrefs.GetInt(highScoreKey,0);
+			//if score is greater, store previous highScore
+			//Set new highScore
+			//set score to previous highScore, and try again
+			//Once score is greater, it will always be for the
+			//remaining list, so the top 5 will always be 
+			//updated
+			if(score>highScore){
+				int temp = highScore;
+				PlayerPrefs.SetInt (highScoreKey, score);
+				score = temp;
+			}
 		}
 	}
 
