a06e08f18dbe2c5c5b75b5dd872be106e3e5ec82
Thomas Tran <thomastran878@gmail.com>
2017-04-15 13:44:00 -0700

    Added cannon, cannon rotation

---
+++ Assets/Thomas-Development/Scripts/SwitchItems.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchItems : MonoBehaviour {
+
+    public GameObject cannon;
+    public GameObject basket;
+    public GameObject pointLine;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire2"))
+        {
+            if (cannon.activeInHierarchy)
+            {
+                cannon.SetActive(false);
+                basket.SetActive(true);
+                pointLine.SetActive(true);
+            }
+            else
+            {
+                cannon.SetActive(true);
+                basket.SetActive(false);
+                pointLine.SetActive(false);
+            }
+        }
+	}
+}
