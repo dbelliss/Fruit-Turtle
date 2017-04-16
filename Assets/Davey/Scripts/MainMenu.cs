2c27b728d443ac34040af9333bd48436cf1df19f
dbelliss <daveybelliss@yahoo.com>
2017-04-15 21:10:41 -0700

    Adding cannon noise

--- Assets/Davey/Scripts/MainMenu.cs
+++ Assets/Davey/Scripts/MainMenu.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
