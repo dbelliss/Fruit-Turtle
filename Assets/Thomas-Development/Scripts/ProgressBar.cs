c9847d1906711223ca66ab9b8f3f19da4f141999
Thomas Tran <thomastran878@gmail.com>
2017-04-15 18:40:58 -0700

    Added working progress bar

---
+++ Assets/Thomas-Development/Scripts/ProgressBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour {
+
+    public Slider slider;
+
+	// Use this for initialization
+	void Start () {
+        slider.maxValue = GameManager.pointsToWin;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        slider.value = GameManager.GetCurrentPoints();
+	}
+}
