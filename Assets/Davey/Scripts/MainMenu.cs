4785938854eba28dba0dd4e8e96be4b3ef183f05
dbelliss <daveybelliss@yahoo.com>
2017-04-15 22:02:49 -0700

    Initialize

--- Assets/Davey/Scripts/MainMenu.cs
+++ Assets/Davey/Scripts/MainMenu.cs
@@ -8,8 +8,22 @@ public class MainMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		InitScoreBoard ();
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
+
+	void InitScoreBoard() {
+		float[] highScores = new float[5];
+		for (int i= 0; i < highScores.Length; i++){
+			string highScoreKey = "HighScore"+(i+1).ToString();
+			string nameKey = "Name" + (i + 1).ToString ();
+			if (PlayerPrefs.GetFloat (highScoreKey) == 0) {
+				PlayerPrefs.SetFloat (highScoreKey, 9999);
+				PlayerPrefs.SetString (nameKey, "Unknown");
+			}
+
+		}
+	}
 
 	// Update is called once per frame
 	void Update () {
