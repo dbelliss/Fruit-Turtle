69ac9b6c9b1c66bb0c6a738267a1e7048d34c96a
Thomas Tran <thomastran878@gmail.com>
2017-04-15 17:03:32 -0700

    Turtle 1.4, new cannonball

---
+++ Assets/Thomas-Development/Scripts/RotateProjectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateProjectile : MonoBehaviour
+{
+    public float grav = 1;
+    private float angularSpeed;
+    private Rigidbody2D rb2d;
+    private Transform trans;
+    // Use this for initialization
+    void Start()
+    {
+        angularSpeed = Random.Range(4f, 10f);
+        trans = GetComponent<Transform>();
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.gravityScale = grav;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotate();
+    }
+
+    void rotate()
+    {
+        trans.Rotate(Vector3.forward * angularSpeed);
+    }
+}
