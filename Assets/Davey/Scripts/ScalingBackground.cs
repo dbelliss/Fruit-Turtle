c9f68d5dfabbb42902e79befcab2650f839c719f
Davey <daveybelliss@gmail.com>
2018-03-21 01:07:32 -0700

    Fixed bug when switching from portrait to landscape on application launch

--- Assets/Davey/Scripts/ScalingBackground.cs
+++ Assets/Davey/Scripts/ScalingBackground.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class ScalingBackground : MonoBehaviour {
 
 	// Scales background to camera size at start
-	void Start () {
+	void Awake () {
         Screen.orientation = ScreenOrientation.LandscapeLeft; // Lock screen to landscape
-        SpriteRenderer sr = GetComponent<SpriteRenderer>();
-
-		float worldScreenHeight = Camera.main.orthographicSize * 2;
-		float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
-
-		transform.localScale = new Vector3(
-			worldScreenWidth / sr.sprite.bounds.size.x,
-			worldScreenHeight / sr.sprite.bounds.size.y, 1);
+        StartCoroutine(SetBackgroundSize());
 	}
+
+    IEnumerator SetBackgroundSize()
+    {
+        while (true)
+        {
+            SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+            float worldScreenHeight = Camera.main.orthographicSize * 2;
+            float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+            transform.localScale = new Vector3(
+                worldScreenWidth / sr.sprite.bounds.size.x,
+                worldScreenHeight / sr.sprite.bounds.size.y, 1);
+            yield return new WaitForSeconds(.5f);
+        }
+
+        
+    }
 }
